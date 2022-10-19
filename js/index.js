6c46e31ca8af4350100c15af43e156bb5f2625a2
leavemealonemf <strangemisterio78@gmail.com>
2022-10-19 21:48:08 +0300

    fullstack

---
+++ js/index.js
@@ -0,0 +1,67 @@
+// Карусель
+
+$(document).ready(function(){
+    $("#slider").owlCarousel({
+        items: 4,
+        margin: 40,
+        loop: true,
+        stagePadding: 80,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        autoplayHoverPause: true,
+        lazyLoad: true,
+        responsiveClass: true,
+        responsive: {
+            1040: {
+              items: 4
+            },
+            992: {
+              items: 4
+            },
+            767: {
+              items: 3
+            },
+            375: {
+              items: 1
+            }
+        }
+    });
+});
+
+// Услуги скролл
+
+let flag = false;
+
+$('#btn1').click(function () {
+    if(!flag){
+     $('#block2').slideDown();
+    }else{
+     $('#block2').slideUp();
+    }
+    flag = !flag;
+});
+
+// Отзывы скролл
+
+$('#btn2').click(function(){
+  if(!flag){
+    $('#review2').slideDown();
+  }else{
+    $('#review2').slideUp();
+  }
+  flag = !flag;
+});
+
+
+// Бургер меню
+
+$(document).ready(function() {
+  $(".menuToggle").click(function() {
+    $(this).toggleClass("active");
+    $('.menu').slideToggle(300, function(){
+      if($(this).css('display') === "none"){
+        $(this).removeAttr('style');
+      }
+    });
+  });
+});
